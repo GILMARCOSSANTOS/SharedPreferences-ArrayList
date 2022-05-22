4afce581aaec7fb1bffcda72311a8b2d8d3e6dd0
Gilmarcos Santos <54557299+GILMARCOSSANTOS@users.noreply.github.com>
2022-05-22 09:43:05 -0300

    Layouts Concluidos e testados

--- app/src/main/java/com/example/sharedpreferecesarraylist/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferecesarraylist/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.sharedpreferecesarraylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Centralizar ActionBar */
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.actionbar_center)
+
+
     }
 }
